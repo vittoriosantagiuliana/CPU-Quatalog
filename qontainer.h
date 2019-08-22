674f81cd8a4415ec3e4d2967b57b9712821fca77
Toyo <vittoriosantagiuliana@gmail.com>
2019-08-22 02:34:40 +0200

    alpha iterators

--- qontainer.h
+++ qontainer.h
@@ -7,8 +7,7 @@ template <class T>
 class Qontainer
 {
 private:
-    DeepPtr<T> * q;
-    int startIndex;
+    T * q;
     int endIndex;
     int totalSize;
 
@@ -19,18 +18,19 @@ public:
 
     unsigned int size() const;
 
+
     class iterator
     {
+        friend class Qontainer;
     private:
         T * p;
+        iterator (T * = nullptr);
     public:
-        iterator Start() const;
-        iterator End() const;
-        iterator (const T *);
-        iterator (const iterator &);
-
         iterator & operator++ ();
-        friend iterator & operator++ (iterator &);
+        iterator operator++ (int);
+        iterator & operator-- ();
+        iterator operator-- (int);
+        T & operator*() const;
         T * operator->() const;
     };
 
@@ -38,22 +38,49 @@ public:
     {
     private:
         const T * p;
+        const_iterator (const T * = nullptr);
     public:
-        const_iterator Start() const;
-        const_iterator End() const;
-
-
-        const T * operator->() const;
+        const_iterator & operator++ ();
+        const_iterator operator++ (int);
+        const_iterator & operator-- ();
+        const_iterator operator-- (int);
+        T & operator*() const;
+        T * operator->() const;
     };
 
+    iterator & begin();
+    iterator & end();
+    const_iterator & cbegin() const;
+    const_iterator & cend() const;
 };
 
 // IMPLEMENTATION
 
+template <class T>
+Qontainer<T>::Qontainer() : q(nullptr), endIndex(0), totalSize(0) { }
 
-template<class T>
-unsigned int Qontainer<T>::size() const {
-    return endIndex - startIndex;
-}
+template <class T>
+unsigned int Qontainer<T>::size() const { return endIndex; }
+
+template <class T>
+typename Qontainer<T>::iterator & Qontainer<T>::begin() { return iterator(q); }
+template <class T>
+typename Qontainer<T>::iterator & Qontainer<T>::end() { return iterator(q + endIndex); }
+template <class T>
+typename Qontainer<T>::const_iterator & Qontainer<T>::cbegin() const { return const_iterator(q); }
+template <class T>
+typename Qontainer<T>::const_iterator & Qontainer<T>::cend() const { return const_iterator(q + endIndex); }
+
+template <class T>
+Qontainer<T>::iterator::iterator(T * d) : p(d) { }
+template <class T>
+typename Qontainer<T>::iterator & Qontainer<T>::iterator::operator++ () { p++; return *this; }
+template <class T>
+typename Qontainer<T>::iterator Qontainer<T>::iterator::operator++ (int) { iterator temp (* this); p++; return temp; }
+
+
+template <class T>
+Qontainer<T>::const_iterator::const_iterator(const T * d) : p(d) { }
+
 
 #endif // QONTAINER_H
